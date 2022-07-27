967be64fe222a4d1bf0ea3842e5a2c0fbc6624f8
Mohamed Mokhtar <Mohamed_201900731@fci.helwan.edu.eg>
2022-07-27 16:43:53 +0200

    .

--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,30 @@ link.forEach(li=>{
         addActive(e);
     })
 })
+
+let felter=document.querySelectorAll(".port .container .filter ul li");
+felter.forEach(ele=>{
+    ele.addEventListener("click",(e)=>{
+        addActive(e)
+        // console.log(e.target)
+        if(e.target.classList.contains("css")){
+            document.querySelectorAll(".html").forEach(e=>{e.style.display="block"})
+            document.querySelectorAll(".java").forEach(e=>{e.style.display="none"})
+        }
+        else if(e.target.classList.contains("javascript")){
+            document.querySelectorAll(".java").forEach(e=>{e.style.display="block"})
+            document.querySelectorAll(".html").forEach(e=>{e.style.display="none"})
+        }
+        else if(e.target.classList.contains("all")){
+            document.querySelectorAll(".box").forEach(e=>{e.style.display="block"})
+        }
+        else if(e.target.classList.contains("games")){
+            document.querySelectorAll(".html").forEach(e=>{e.style.display="none"})
+            document.querySelectorAll(".java").forEach(e=>{e.style.display="none"})
+            
+
+        }
+    })
+})
+
+
